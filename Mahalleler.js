e76b7790d6e40708811effd24f9cddc7859d9573
furukcell <kurtulusfaruk94@gmail.com>
2026-05-09 10:18:00 +0300

    Update Mahalleler.js

--- Mahalleler.js
+++ Mahalleler.js
@@ -1,1 +1,162 @@
-
+export const MAHALLE_HIYERARSISI = {
+  "Menteşe": {
+    "Merkez Mahalleler": [
+      "Balıbey", "Camikebir", "Emirbeyazıt", "Hacırüstem", "Karamehmet", 
+      "Karamuğla", "Kiramettin", "Muslihittin", "Müştakbey", "Orhaniye", 
+      "Ortaköy", "Şeyh", "Yeniköy"
+    ],
+    "Köyler / Beldeler": [
+      "Akçaova", "Akkaya", "Akyer", "Algı", "Avcılar", "Bağyaka", "Bayır", 
+      "Bozyer", "Çakmak", "Çamoluk", "Çaybükü", "Çırpı", "Çiftlik", "Dağdibi", 
+      "Dağpınar", "Denizova", "Derinkuyu", "Doğan", "Dokuzçam", "Düğerek", 
+      "Esençay", "Fadıl", "Gazeller", "Göktepe", "Gülağzı", "Günlüce", "İkizce", 
+      "Kafaca", "Karacaören", "Kıran", "Kömürlük", "Kozluk", "Kuyucak", "Meke", 
+      "Muratlar", "Özlüce", "Paşapınarı", "Salihpaşalar", "Sarnıç", "Sungur", 
+      "Şenyayla", "Taşlı", "Tengerek", "Yemişendere", "Yenibağyaka", "Yenice", 
+      "Yerkesik", "Yeşilyurt", "Zeytinköy"
+    ]
+  },
+  "Bodrum": {
+    "Merkez Mahalleler": [
+      "Çarşı", "Cevat Şakir", "Cumhuriyet", "Eskiçeşme", "Gümbet", "Kumbahçe", 
+      "Tepecik", "Türkkuyusu", "Umurça", "Yeniköy", "Yokuşbaşı"
+    ],
+    "Köyler / Beldeler": [
+      "Akyarlar", "Bahçeyaka", "Bitez", "Bozalan", "Çamarası", "Çamlık", "Çırkan", 
+      "Çiftlik", "Çömlekçi", "Dağbelen", "Dereköy", "Dirmil", "Gökpınar", "Göltürkbükü", 
+      "Gölbaşı", "Gümüşlük", "Gündoğan", "Gürece", "Güvercinlik", "İslamhaneleri", 
+      "Kemer", "Konacık", "Koyunbaba", "Kızılağaç", "Kumköy", "Kurudere", "Mazı", 
+      "Mumcular", "Müskebi", "Ortakent", "Peksimet", "Pınarlıbelen", "Sazköy", 
+      "Torba", "Turgutreis", "Türkbükü", "Yakapelit", "Yalıkavak", "Yahşi"
+    ]
+  },
+  "Dalaman": {
+    "Merkez Mahalleler": [
+      "Altıntas", "Atakent", "Bezkese", "Ege", "Karaçalı", "Merkez", "Söğütlüyurt", "Hürriyet"
+    ],
+    "Köyler / Beldeler": [
+      "Akçataş", "Bozbel", "Çöğmen", "Darısu", "Elcik", "Gürleyik", "Gürköy", 
+      "Kapukargın", "Kavacık", "Kayadibi", "Kille", "Kızılkaya", "Narlı", 
+      "Sarsala", "Şerefler", "Taşbaşı"
+    ]
+  },
+  "Datça": {
+    "Merkez Mahalleler": [
+      "İskele", "Eski Datça", "Reşadiye", "Datça"
+    ],
+    "Köyler / Beldeler": [
+      "Cumalı", "Emecik", "Hızırşah", "Karaköy", "Kızlan", "Mesudiye", 
+      "Palamutbükü", "Sındı", "Yaka", "Yazıköy"
+    ]
+  },
+  "Fethiye": {
+    "Merkez Mahalleler": [
+      "Akarca", "Babataşı", "Cami", "Cumhuriyet", "Çatalarık", "Foça", "Karagözler", 
+      "Kesikkapı", "Menteşeoğlu", "Pazaryeri", "Taşyaka", "Tuzla", "Yeni Mahalle"
+    ],
+    "Köyler / Beldeler": [
+      "Arpacık", "Bayırköy", "Bozyer", "Çamköy", "Çiftlik", "Eldeniz", "Esenköy", 
+      "Faralya", "Gökben", "Gökçeovacık", "Göcek", "Hacılar", "İncirköy", "İnlice", 
+      "Karaağaç", "Karacaören", "Karaçulha", "Kargı", "Kayıköy", "Kızılbel", 
+      "Koruköy", "Nif", "Ölüdeniz", "Söğütlü", "Uzunyurt", "Yakacık", "Yanıklar"
+    ]
+  },
+  "Kavaklıdere": {
+    "Merkez Mahalleler": [
+      "Cumhuriyet", "Yeni Mahalle", "Bakırcılar"
+    ],
+    "Köyler / Beldeler": [
+      "Çamlıbel", "Çayboyu", "Derebağ", "Kurucuova", "Menteşe (Kavaklıdere)", 
+      "Nebiler", "Ortaköy", "Salkım", "Yeşilköy"
+    ]
+  },
+  "Köyceğiz": {
+    "Merkez Mahalleler": [
+      "Gelişim", "Gülpınar", "Uluerpınar", "Yeni Mahalle"
+    ],
+    "Köyler / Beldeler": [
+      "Akçapınar", "Balcılar", "Beyobası", "Çandır", "Döğüşbelen", "Ekincik", "Hamitköy", 
+      "Karaoğlanlar", "Kavakarası", "Köyceğiz (Köy)", "Otmanlar", "Pınar", "Sazak", 
+      "Toparlar", "Yangı", "Yayla", "Zeytinalanı"
+    ]
+  },
+  "Marmaris": {
+    "Merkez Mahalleler": [
+      "Armutalan", "Beldibi", "Çıldır", "Hatipirimi", "Kemeraltı", "Sarıana", "Siteler", "Tepe"
+    ],
+    "Köyler / Beldeler": [
+      "Adaköy", "Bayır", "Bozburun", "Çamlı", "Çetibeli", "Hisarönü", "İçmeler", "Karaca", 
+      "Orhaniye", "Osmaniye", "Selimiye", "Söğüt", "Taşlıca", "Turgut", "Turunç", "Yeşilbelde"
+    ]
+  },
+  "Milas": {
+    "Merkez Mahalleler": [
+      "Aydınlıkevler", "Burgaz", "Cumhuriyet", "Emek", "Firuzpaşa", "Gümüşlük", "Güneş", "Hacı İlyas", 
+      "Hacıabti", "Hayıtlı", "Hisarbaşı-Hoca Bedrettin", "İsmet Paşa", "Şevketiye"
+    ],
+    "Köyler / Beldeler": [
+      "Ağaçlıhüyük", "Akçakaya", "Akçalı", "Akkovanlık", "Akyol", "Alaçam", "Alatepe", 
+      "Aslanyaka", "Avşar", "Bafa", "Bağdamları", "Baharlı", "Bahçe", "Bahçeburun", 
+      "Balcılar", "Bayır", "Beçin", "Beyciler", "Boğaziçi", "Bozalan", "Bozbük", 
+      "Çamköy", "Çamlıbelen", "Çamlıca", "Çamlıyurt", "Çandır", "Çınarlı", "Çiftlik", 
+      "Çomakdağ Kızılağaç", "Çukur", "Danişment", "Demirciler", "Dere", "Derince", 
+      "Dibekdere", "Dörttepe", "Eğridere", "Ekinanbarı", "Ekindere", "Ekizköy", "Ekiztaş", 
+      "Epçe", "Etrenli", "Garipler", "Gökçeler", "Göldere", "Gölyaka", "Gülçukur", 
+      "Güllük", "Gürceğiz", "Gürçamlar", "Hacıahmetler", "Hasanlar", "Hisarcık", 
+      "Hüsamlar", "İçme", "İkizce", "İkiztaş", "İlmin", "Kalem", "Kalınağıl", "Kandilli", 
+      "Kapıkırı", "Karacaağaç", "Karacahisar", "Karagömlek", "Karahayıt", "Karakuyu", 
+      "Karapınar", "Kargıcak", "Karşıyaka", "Kayabaşı", "Kayabükü", "Kaymakkavak", 
+      "Kazıklı", "Kemikler", "Ketendere", "Kılavuz", "Kırcağız", "Kısırlar", "Kıyıkışlacık", 
+      "Kızılağaç", "Kızılcakuyu", "Kızılcayıkık", "Konak", "Korucuk", "Koruderesi", "Köşk", 
+      "Koru", "Kurudere", "Kuzyaka", "Küçükdibekdere", "Menteş", "Meşelik", "Narhisar", 
+      "Olukbaşı", "Ortaköy", "Ovakışlacık", "Ören", "Pinar", "Pınarcık", "Pınarköy", 
+      "Sakarkaya", "Sarıkaya", "Savran", "Selimiye", "Söğütçük", "Şenköy", "Şeyh", 
+      "Türkevleri", "Tuzabat", "Ulaş", "Yaka", "Yakaköy", "Yaşyer", "Yoğunoluk", "Yusufça"
+    ]
+  },
+  "Ortaca": {
+    "Merkez Mahalleler": [
+      "Atatürk", "Beşköprü", "Cumhuriyet", "Dikmekavak", "Terzialiler", "Yerbelen"
+    ],
+    "Köyler / Beldeler": [
+      "Akıncı", "Arıkbaşı", "Bahçelievler", "Dalyan", "Ekşiliyurt", "Eskiköy", "Fevziye", 
+      "Gökbel", "Gölbaşı", "Güzelyurt", "Karaburun", "Kemaliye", "Kocabel", "Mergenli", 
+      "Okçular", "Tepearası"
+    ]
+  },
+  "Seydikemer": {
+    "Merkez Mahalleler": [
+      "Cumhuriyet", "Gerişburnu", "Menekşe"
+    ],
+    "Köyler / Beldeler": [
+      "Alaçat", "Arifler", "Arsa", "Atlıdere", "Bağlıağaç", "Bayırköy", "Belen", 
+      "Boğalar", "Bozburun", "Ceylan", "Çatak", "Çayan", "Çaykenarı", "Çobanlar", 
+      "Çobanisa", "Çukurincir", "Dereköy", "Dodurga", "Doğanlar", "Dont", "Eşen", 
+      "Gökçeören", "Gölbent", "Güneşli", "Hacıosmanlar", "İzzettin", "Kabaağaç", 
+      "Kadıköy", "Karahayıt", "Karaköy", "Karadere", "Kayabaşı", "Kayacık", "Kıncılar", 
+      "Korubükü", "Kumluova", "Minare", "Ören", "Paşalı", "Sahilceylan", "Sarıyer", 
+      "Seki", "Seydiler", "Söğütlüdere", "Step", "Temel", "Uğurlu", "Yakabağ", "Yayla Patlangıç", "Zorlar"
+    ]
+  },
+  "Ula": {
+    "Merkez Mahalleler": [
+      "Alparslan", "Ayazma", "Köprübaşı"
+    ],
+    "Köyler / Beldeler": [
+      "Akçapınar", "Akyaka", "Arıcılar", "Ataköy", "Çakırhan", "Çiçekli", "Elmalı", 
+      "Esentepe", "Gökçe", "Gökova", "Gölcük", "Kavakçalı", "Kızılağaç", "Kızılyaka", 
+      "Ovakışla", "Sarayyanı", "Şirinköy", "Turgut", "Yaylasöğüt", "Yeşilova"
+    ]
+  },
+  "Yatağan": {
+    "Merkez Mahalleler": [
+      "Akyol", "Atatürk", "Cumhuriyet", "Konak", "Yeni Mahalle"
+    ],
+    "Köyler / Beldeler": [
+      "Alaşar", "Bağyaka", "Bencik", "Bozarmut", "Bozüyük", "Çakırhan", "Çamlıca", 
+      "Dere", "Elmacık", "Eskihisar", "Gökpınar", "Hacıbayramlar", "Hacıveliler", 
+      "Kafacakaplancık", "Kapubağ", "Katrancı", "Kozağaç", "Madenler", "Nebiköy", 
+      "Şahinler", "Turgut", "Turgutlar", "Yava", "Yeşilbağcılar", "Yukarıyayla"
+    ]
+  }
+};
