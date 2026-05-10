0557d550afaa623f91d1e7e1460f61f16ffda69d
furukcell <kurtulusfaruk94@gmail.com>
2026-05-10 22:24:59 +0300

    Update App.js

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import { AnasayfaEkrani, SolMenu } from './HomeScreen';
 import { IlanVerEkrani, IlanlarimEkrani, TeklifVerEkrani, TekliflerEkrani } from './IlanScreens';
 import { SohbetEkrani } from './ChatScreen';
 import { ProfilEkrani } from './ProfileScreens';
-import { OdemeEkrani, DavetEkrani, AyarlarEkrani, IletisimEkrani, HakkimizdaEkrani, HizmetKosullariEkrani, BildirimEkrani } from './PackageScreens';
+import { OdemeEkrani, DavetEkrani, AyarlarEkrani, IletisimEkrani, HakkimizdaEkrani, HizmetKosullariEkrani, BildirimEkrani, revenueCatBaslat } from './PackageScreens';
 import { PuanModali, SikayetModali } from './Modals';
 import { AdminEkrani } from './AdminScreen';
 
@@ -24,6 +24,7 @@ import { pushTokenAl } from './notifications';
 import * as Updates from 'expo-updates';
 
 // Bildirim ayarı
+revenueCatBaslat();
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
