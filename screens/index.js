3fd9d008116c46854c35cdb4f1c0f2b93ae95bab
furukcell <kurtulusfaruk94@gmail.com>
2026-05-16 14:51:47 +0300

    Update index.js

--- screens/index.js
+++ screens/index.js
@@ -6,10 +6,19 @@
 // ============================================================
 
 export { revenueCatBaslat, REVENUECAT_API_KEY, PAKET_ID_MAP } from './revenueCat';
-export { OdemeEkrani }         from './OdemeEkrani';
-export { DavetEkrani }         from './DavetEkrani';
-export { AyarlarEkrani }       from './AyarlarEkrani';
-export { IletisimEkrani }      from './IletisimEkrani';
-export { HakkimizdaEkrani }    from './HakkimizdaEkrani';
+export { OdemeEkrani }           from './OdemeEkrani';
+export { DavetEkrani }           from './DavetEkrani';
+export { AyarlarEkrani }         from './AyarlarEkrani';
+export { IletisimEkrani }        from './IletisimEkrani';
+export { HakkimizdaEkrani }      from './HakkimizdaEkrani';
 export { HizmetKosullariEkrani } from './HizmetKosullariEkrani';
-export { BildirimEkrani }      from './BildirimEkrani';
+export { BildirimEkrani }        from './BildirimEkrani';
+export { AdminScreen }           from './AdminScreen';
+export { AuthScreens }           from './AuthScreens';
+export { ChatScreen }            from './ChatScreen';
+export { HomeScreen }            from './HomeScreen';
+export { IlanScreens }           from './IlanScreens';
+export { KvkkEkrani }            from './KvkkEkrani';
+export { Modals }                from './Modals';
+export { ProfileScreens }        from './ProfileScreens';
+export { SifremiUnuttumEkrani }   from './SifremiUnuttumEkrani';
