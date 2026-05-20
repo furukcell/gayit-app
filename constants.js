4bb6e72f75fcc326e7a863725c5b185f9a319502
furukcell <kurtulusfaruk94@gmail.com>
2026-05-20 07:50:20 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -7,7 +7,7 @@
 export const API_KEY = "AIzaSyCcvq9VkMugDZTq3fOPypJIy0ATiGmPxrk";
 export const DB_URL = "https://usta-mugla-default-rtdb.europe-west1.firebasedatabase.app";
 export const STORAGE_BUCKET = "usta-mugla.appspot.com";
-
+export const FIREBASE_API_KEY = "AIzaSyCcvq9VkMugDZTq3fOPypJIy0ATiGmPxrk";
 // --- Sabit Listeler ---
 export const BOLGELER = [
   'Menteşe (Merkez)', 'Bodrum', 'Dalaman', 'Datça', 'Fethiye',
