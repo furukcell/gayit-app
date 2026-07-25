579398f253e8d9368d7f282c8ffea5883faea2af
furukcell <kurtulusfaruk94@gmail.com>
2026-07-25 12:59:17 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -36,6 +36,11 @@ export const KATEGORILER = [
   'Halı & Koltuk Yıkama',
   'Çatı & Su Yalıtımı',
   'Fotoğrafçı & Kameraman',
+  'Saç kaynak',
+  'İlaçlama',
+  'Perde',
+  'Ev yemekleri',
+  'Hasta/Çocuk bakıcısı',
   'Güneş Enerji Sistemi',
   'Özel Ders & Eğitim',
   // ------------------------
